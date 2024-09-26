d3d31ce16a84cbba9710dee216d86b9101e80e46
Mukhtar Yahaya <yahayamukhtar.m@gmail.com>
2024-09-26 18:13:54 +0100

    use fabacae family

--- impact_indicator.R
+++ impact_indicator.R
@@ -9,11 +9,12 @@ grid <- rsa_country_sf %>%
   sf::st_sf() %>%
   dplyr::mutate(cellid = dplyr::row_number())
 
-taxon.sf = gbif_download$data %>%
+taxon.sf = taxa_Fabacae %>%
   dplyr::select(decimalLatitude,decimalLongitude,
                 species,coordinateUncertaintyInMeters,year,speciesKey) %>% #select occurrence data
-  filter_all(all_vars(!is.na(.))) %>% # remove rows with missing data
-  filter(coordinateUncertaintyInMeters<=res*1000) %>% 
+  #filter_all(all_vars(!is.na(.))) %>% # remove rows with missing data
+  #filter(coordinateUncertaintyInMeters<=res*1000) %>% 
+  filter(!is.na(decimalLatitude)) %>% 
   sf::st_as_sf(coords = c("decimalLongitude", "decimalLatitude"),
                crs = 4326) %>%
   st_join(grid) %>% 
@@ -26,6 +27,21 @@ taxon.sf = gbif_download$data %>%
 taxon_cube<-process_cube(taxon.sf,grid_type = "custom",cols_cellCode = "cellid", cols_year = "year",
                  cols_occurrences = "occurrenceStatus",cols_species = "species",cols_speciesKey = "speciesKey",
                  cols_minCoordinateUncertaintyInMeters = "coordinateUncertaintyInMeters")
+
+
+
+ts_obs_rich <- obs_richness_ts(taxon_cube, first_year=1990)
+plot(ts_obs_rich)
+list_species(taxon_cube)
+ts_turnover<-occ_turnover_ts(taxon_cube)
+
+plot(ts_turnover)
+ts_density<-occ_density_ts(taxon_cube)
+plot(ts_density)
+
+ts_evenness<-pielou_evenness_ts(taxon_cube)
+
+plot(ts_evenness)
 
 sbs.taxon<-taxon_cube$data %>%
   # select species name, trait and trait value
@@ -132,18 +148,17 @@ eicat_impact<-function(eicat_data,
   names(na.df)<-names(category_M) # column names
   category_M<-rbind(category_M,na.df)
   category_M<-category_M[order(rownames(category_M)),]
-  #category_M<-as.matrix(category_M)
   return(category_M)
     
     
 }
 
-Eicat_score=eicat_impact(eicat_data = eicat_data,species_list = species_list,
-                         fun="avg")
-Eicat_score
+eicat_score=eicat_impact(eicat_data = eicat_data,species_list = species_list,
+                         fun="max")
+eicat_score
 site_pressure<-status.sf$intro_native
 
-abdundance_impact = sweep(sbs.taxon,2,category_M,FUN = "*")
+abdundance_impact = sweep(sbs.taxon,2,eicat_score,FUN = "*")
 impactScore = site_pressure*abdundance_impact
 
 
@@ -153,45 +168,7 @@ specieImpact<-specieImpact[specieImpact>0]
 siteImpact<-rowSums(impactScore,na.rm = TRUE)
 siteImpact<-siteImpact[siteImpact>0]
 
-if(fun=="max"){
-  f<-function(x) max(x,na.rm = T)
-} else if(fun=="min"){
-  f<-function(x) min(x,na.rm = T)
-} else if(fun=="mean"){
-  f<-function(x) mean(x,na.rm = T)
-} else {stop("`fun` should be max, min or mean character")}
 
-category_M = eicat_data %>% 
-  mutate(impact_category=substr(impact_category,1,2)) %>% 
-  filter(impact_category %in% c("MC","MN","MO","MR","MV")) %>% 
-  select(scientific_name,
-         impact_category) %>% 
-  mutate(category_value = case_when(
-    impact_category == "MC" ~ 0,
-    impact_category == "MN" ~1,
-    impact_category == "MO" ~2,
-    impact_category == "MR" ~3,
-    impact_category == "MV" ~4,
-    TRUE ~ 0  # Default case, if any value falls outside the specified ranges
-  )) %>% 
-  group_by(scientific_name,impact_category) %>%
-  #choose the first trait value if there are multiples trait for a species
-  summarise(across(category_value, first), .groups = "drop") %>% 
-  # reshape to wide format to have specie by trait dataframe
-  pivot_wider(names_from = impact_category, values_from = category_value) %>% 
-  # select species that are only present in gbif data
-  filter(scientific_name %in% species_list) %>% 
-  #convert species names to row names
-  column_to_rownames(var = "scientific_name") 
 
   
-  category_M<-data.frame("fun"=apply(category_M,1,f))
-  names(category_M)<-fun
-na.df<-as.data.frame(matrix(NA,nrow = length(setdiff(species_list,rownames(category_M))),
-                            ncol = ncol(category_M)))
-row.names(na.df)<-setdiff(species_list,rownames(category_M))
-names(na.df)<-names(category_M) # column names
-category_M<-rbind(category_M,na.df)
-category_M<-category_M[order(rownames(category_M)),]
-category_M<-as.matrix(category_M)
 
