e64366dc7a82d1437ddffbf8d6ddccee091884c3
Mukhtar Yahaya <yahayamukhtar.m@gmail.com>
2024-10-01 15:50:31 +0100

    first test

--- impact_bootstrap.R
+++ impact_bootstrap.R
@@ -9,7 +9,7 @@ sbs.fun<-function(y){
     column_to_rownames(var = "cellCode")  #%>% 
     #mutate(across(all_of(everything()), ~ ifelse(is.na(.),0,.)))
     
-  colnames(sbs.taxon)<-NULL
+  #colnames(sbs.taxon)<-NULL
   sbs.taxon<-as.matrix(sbs.taxon)
   return(sbs.taxon)
 }
@@ -22,12 +22,49 @@ my_boot_statistic <- function(data, indices, fun) {
 
 my_fun<-function(x){
   
-  m=mean(x,na.rm=TRUE)
+  species_list<-names(x)
 
-  if (is.nan(m)) {
-    m <- NA
+  if(!exists("taxon_status_list")){
+    full_species_list<-sort(unique(taxon_cube$data$scientificName))
+    taxon_status_list<-taxon_status(species_list = full_species_list,
+                                    source = "WCVP",
+                                    region = "South Africa")
   }
-   return(m)
+
+  intro.sf<-taxon_cube$data %>%
+    filter(year==y) %>%
+    left_join(taxa_list_status,
+              by = c("scientificName" = "taxon"))
+
+
+  status.sf <- intro.sf %>%
+    group_by(cellCode) %>%
+    summarise(
+      total_intro_obs = sum(obs[introduction_status == "introduced"], na.rm = TRUE),
+      total_native_obs = sum(obs[introduction_status == "native"], na.rm = TRUE),
+      .groups = "drop"
+    ) %>%
+    mutate(across(c(total_intro_obs, total_native_obs), ~ ifelse(.==0,NA,.))) %>%
+    mutate(intro_native=total_intro_obs/total_native_obs) %>%
+    arrange(cellCode)
+  if (!exists("eicat_score_list")){
+    eicat_score_list=eicat_impact(eicat_data = eicat_data,species_list = species_list,
+                                  fun="max")
+  }
+  
+  eicat_score<-eicat_score_list[species_list,]
+  
+  siteScore<-status.sf$intro_native
+  abdundance_impact = sweep(sbs.taxon,2,eicat_score,FUN = "*")
+  impactScore = siteScore*abdundance_impact
+  impact<-sum(impactScore,na.rm = TRUE) 
+  
+  
+  # 
+  # if (is.nan(m)) {
+  #   m <- NA
+  # }
+   return(impact)
 }
 
 sbs.taxon_list<-map(period[-c(1:12)],sbs.fun)
@@ -37,7 +74,7 @@ sbs.taxon_list<-map(period[-c(1:12)],sbs.fun)
 
 
 fun=my_fun
-samples<-1000
+samples<-10
 bootstrap_list <- map(period[-c(1:12)],sbs.fun) %>% 
   # Perform bootstrapping
   purrr::map(~boot::boot(
