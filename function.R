c195f31608cce6194191758d992bb9eabec85472
Mukhtar Yahaya <yahayamukhtar.m@gmail.com>
2024-10-03 17:12:01 +0100

    taxaFUN return data cubes

--- function.R
+++ function.R
@@ -10,7 +10,14 @@ library(rWCVP)
 library(rWCVPdata)
 
 
-taxaFun <- function(taxa,limit=500, ref=NULL,country='ZA',res=0.25){
+taxaFun <- function(taxa,country.sf,limit=500, ref=NULL,country='ZA',res=0.25){
+  
+  grid <- country.sf %>%
+    sf::st_make_grid(cellsize = c(res,res),
+                     offset = c(sf::st_bbox(country.sf)$xmin,
+                                sf::st_bbox(country.sf)$ymin)) %>%
+    sf::st_sf() %>%
+    dplyr::mutate(cellid = dplyr::row_number())
   
   # download taxa (target taxa) if the scientific name is given as character
   if("character" %in% class(taxa)){
@@ -21,11 +28,11 @@ taxaFun <- function(taxa,limit=500, ref=NULL,country='ZA',res=0.25){
                                   limit = limit)
 
     taxa.df = as.data.frame(taxa.gbif_download$data) #extract data from the downloaded file
-  } else if("data.frame" %in% class(taxa)){ #check if data fame contains the required colums
+  } else if("data.frame" %in% class(taxa)){ #check if data fame contains the required columns
     if(any(!c("decimalLatitude","decimalLongitude",
-              "species","coordinateUncertaintyInMeters","dateIdentified") %in% colnames(taxa))){
-      requiredcol<-c("decimalLatitude","decimalLongitude","species","coordinateUncertaintyInMeters","dateIdentified")
-      missingcol<-requiredcol[!c("decimalLatitude","decimalLongitude","species","coordinateUncertaintyInMeters","dateIdentified") %in% colnames(taxa)]
+              "species","speciesKey","coordinateUncertaintyInMeters","dateIdentified","year") %in% colnames(taxa))){
+      requiredcol<-c("decimalLatitude","decimalLongitude","species","coordinateUncertaintyInMeters","dateIdentified","year")
+      missingcol<-requiredcol[!c("decimalLatitude","decimalLongitude","species","speciesKey","coordinateUncertaintyInMeters","dateIdentified","year") %in% colnames(taxa)]
       cli::cli_abort(c("{missingcol} is/are not in the {.var taxa} column ", 
                       "x" = "{.var taxa} should be a data of GBIF format "))
     }
@@ -36,15 +43,26 @@ taxaFun <- function(taxa,limit=500, ref=NULL,country='ZA',res=0.25){
   }
 
   
-  taxa.df = taxa.df %>%
+  taxa.sf = taxa.df %>%
     dplyr::select(decimalLatitude,decimalLongitude,
-                  species,coordinateUncertaintyInMeters,dateIdentified) %>% #select occurrence data
-    filter_all(all_vars(!is.na(.))) %>% # remove rows with missing data
+                  species,speciesKey,coordinateUncertaintyInMeters,dateIdentified,year) %>% #select occurrence data
+    dplyr::filter_all(all_vars(!is.na(.))) %>% # remove rows with missing data
     dplyr::filter(coordinateUncertaintyInMeters<=res*1000) %>% 
-    dplyr::mutate(coordinateUncertaintyInMeters = coordinateUncertaintyInMeters/(res*1000)^2) %>% 
-    dplyr::mutate(dateIdentified = as.Date(dateIdentified)) # convert date to date format
-  taxa.sf<-sf::st_as_sf(taxa.df,coords = c("decimalLongitude", "decimalLatitude"),
-                    crs = 4326)
+    #dplyr::mutate(coordinateUncertaintyInMeters = coordinateUncertaintyInMeters/(res*1000)^2) %>% 
+    dplyr::mutate(dateIdentified = as.Date(dateIdentified)) %>%  # convert date to date format
+    sf::st_as_sf(coords = c("decimalLongitude", "decimalLatitude"),
+                    crs = 4326) %>% 
+    st_join(grid) %>% 
+    as.data.frame() %>% 
+    dplyr::select(-geometry) %>% 
+    dplyr::mutate(occurrenceStatus=1)
+    
+    
+    taxa_cube<-b3gbi::process_cube(taxa.sf,grid_type = "custom",
+                                    cols_cellCode = "cellid", cols_year = "year",
+                                    cols_occurrences = "occurrenceStatus",
+                                    cols_species = "species",cols_speciesKey = "speciesKey",
+                                    cols_minCoordinateUncertaintyInMeters = "coordinateUncertaintyInMeters")
   # download reference taxa if provided or use the target taxa if otherwise
   if(!is.null(ref)){
     if("character" %in% class(ref)){
@@ -56,39 +74,64 @@ taxaFun <- function(taxa,limit=500, ref=NULL,country='ZA',res=0.25){
       
       ref.df = as.data.frame(ref.gbif_download$data)
       
-      ref.df = ref.df %>%
+      ref.sf = ref.df %>%
         dplyr::select(decimalLatitude,decimalLongitude,
-                      species,coordinateUncertaintyInMeters,dateIdentified) %>% #select occurrence data
-        filter_all(all_vars(!is.na(.))) %>% # remove rows with missing data
-        filter(coordinateUncertaintyInMeters<=res*1000) %>% 
-        mutate(dateIdentified = as.Date(dateIdentified)) # convert date to date format
-      ref.sf<-sf::st_as_sf(ref.df,coords = c("decimalLongitude", "decimalLatitude"),
-                       crs = 4326)
+                      species,speciesKey,coordinateUncertaintyInMeters,dateIdentified,year) %>% #select occurrence data
+        dplyr::filter_all(all_vars(!is.na(.))) %>% # remove rows with missing data
+        dplyr::filter(coordinateUncertaintyInMeters<=res*1000) %>% 
+        dplyr::mutate(dateIdentified = as.Date(dateIdentified)) %>% # convert date to date format
+        sf::st_as_sf(coords = c("decimalLongitude", "decimalLatitude"),
+                       crs = 4326) %>% 
+        st_join(grid) %>% 
+        as.data.frame() %>% 
+        dplyr::select(-geometry) %>% 
+        dplyr::mutate(occurrenceStatus=1)
+      
+      
+        ref_cube<-b3gbi::process_cube(ref.sf,grid_type = "custom",
+                                     cols_cellCode = "cellid", cols_year = "year",
+                                     cols_occurrences = "occurrenceStatus",
+                                     cols_species = "species",cols_speciesKey = "speciesKey",
+                                     cols_minCoordinateUncertaintyInMeters = "coordinateUncertaintyInMeters")
     } else if("data.frame" %in% class(ref)){ # check columns of ref if provided
       if(any(!c("decimalLatitude","decimalLongitude",
-                "species","dateIdentified") %in% colnames(ref))){
-        requiredcol<-c("decimalLatitude","decimalLongitude","species","dateIdentified")
-        missingcol<-requiredcol[!c("decimalLatitude","decimalLongitude","species","dateIdentified") %in% colnames(ref)]
+                "species","speciesKey","coordinateUncertaintyInMeters","dateIdentified","year") %in% colnames(ref))){
+        requiredcol<-c("decimalLatitude","decimalLongitude","species","speciesKey","coordinateUncertaintyInMeters","dateIdentified","year")
+        missingcol<-requiredcol[!c("decimalLatitude","decimalLongitude","species","speciesKey","coordinateUncertaintyInMeters","dateIdentified","year") %in% colnames(ref)]
         cli::cli_abort(c("{missingcol} is/are not in the {.var ref} column ", 
                          "x" = "{.var ref} should be a data of GBIF format "))
       }
-      ref = ref %>%
+      ref.sf = ref %>%
         dplyr::select(decimalLatitude,decimalLongitude,
-                      species,coordinateUncertaintyInMeters,dateIdentified) %>% #select occurrence data
-        filter_all(all_vars(!is.na(.))) %>% # remove rows with missing data
-        filter(coordinateUncertaintyInMeters<=res*1000) %>%
-        mutate(dateIdentified = as.Date(dateIdentified)) # convert date to date format
-      ref.sf<-sf::st_as_sf(ref,coords = c("decimalLongitude", "decimalLatitude"),
-                       crs = 4326)
+                      species,speciesKey,coordinateUncertaintyInMeters,dateIdentified,year) %>% #select occurrence data
+        dplyr::filter_all(all_vars(!is.na(.))) %>% # remove rows with missing data
+        dplyr::filter(coordinateUncertaintyInMeters<=res*1000) %>%
+        dplyr::mutate(dateIdentified = as.Date(dateIdentified)) %>% # convert date to date format
+        sf::st_as_sf(coords = c("decimalLongitude", "decimalLatitude"),
+                       crs = 4326) %>% 
+        st_join(grid) %>% 
+        as.data.frame() %>% 
+        dplyr::select(-geometry) %>% 
+        dplyr::mutate(occurrenceStatus=1)
+      
+      
+      ref_cube<-b3gbi::process_cube(ref.sf,grid_type = "custom",
+                                    cols_cellCode = "cellid", cols_year = "year",
+                                    cols_occurrences = "occurrenceStatus",
+                                    cols_species = "species",cols_speciesKey = "speciesKey",
+                                    cols_minCoordinateUncertaintyInMeters = "coordinateUncertaintyInMeters")
       
       } else { # stop and report if taxa is not a scientific name or dataframe
         cli::cli_abort(c("{.var ref} is not a character or dataframe"))
       }
 
   } else {
-    ref.sf=taxa.sf
+    ref_cube <- taxa_cube
   }
-  return(list("taxa"=taxa.sf,"ref"=ref.sf))
+  
+  
+  
+  return(list("taxa"=taxa_cube,"ref"=ref_cube))
 }
 
 # Specie by site
